87b826d26256b9f49250098d2f29985dd0486221
r.toledo <r.toledo@archriodon.net>
2019-10-15 17:23:54 +0400

    fixed the navbar on mobile screen

--- src/components/layouts/navbar.vue
+++ src/components/layouts/navbar.vue
@@ -1,13 +1,26 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary" v-bind:class=" { 'navbarOpen': show }">
         <router-link to="/" class="navbar-brand">Budget App</router-link>    
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <button 
+            class="navbar-toggler" 
+            type="button" 
+            data-toggle="collapse" 
+            data-target="#navbarColor01" 
+            aria-controls="navbarColor01" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation"
+            v-on:click="toggleNavbar"
+        >
             <span class="navbar-toggler-icon"></span>
         </button>
 
-      <div class="collapse navbar-collapse" id="navbarColor01">
+      <div 
+        class="collapse navbar-collapse" 
+        id="navbarColor01"
+        v-bind:class="{ 'show': show }"
+      >
         <ul class="navbar-nav ml-auto">
-            <li v-if="isLoggedIn"><p class="mt-2 pr-3"><strong>{{currentUser}}</strong></p></li>
+            <li v-if="isLoggedIn" class="nav-item"><p class="mt-2 pr-3"><strong>{{currentUser}}</strong></p></li>
             <li v-if="!isLoggedIn" class="nav-item">
                 <router-link to="/login" class="nav-link">Login</router-link>    
             </li>
@@ -18,7 +31,7 @@
                 <a class="nav-link text-right" href="#" @click="logout">Logout</a>
             </li>
         </ul>
-    </div>
+      </div>
     </nav>
 </template>
 
@@ -31,6 +44,7 @@ export default {
         return {
             isLoggedIn: false,
             currentUser: false,
+            show: true,
         }
     },
     created(){
@@ -44,6 +58,9 @@ export default {
             db.app.auth().signOut().then(() => {
                 this.$router.go({path: this.$router.path})
             })
+        },
+        toggleNavbar() {
+            this.show = !this.show;
         }
     }
 }
